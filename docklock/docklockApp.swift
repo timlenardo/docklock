c9ffdc3652311756651e0c6cdca9a8804f455626
timl <tim.lenardo@gmail.com>
2025-10-27 15:15:55 -0700

    Initial Commit

---
+++ docklock/docklockApp.swift
@@ -0,0 +1,17 @@
+//
+//  docklockApp.swift
+//  docklock
+//
+//  Created by Timothy Lenardo on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct docklockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ docklockTests/docklockTests.swift
@@ -0,0 +1,16 @@
+//
+//  docklockTests.swift
+//  docklockTests
+//
+//  Created by Timothy Lenardo on 10/27/25.
+//
+
+import Testing
+
+struct docklockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
